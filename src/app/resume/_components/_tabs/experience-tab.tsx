469fddd5cd7e056943de1233dc745054b51568f8
= <=>
2025-11-25 19:17:11 +0200

    refactor: remove unused AnimatedStaggerParent component and simplify experience tab layout

--- src/app/resume/_components/_tabs/experience-tab.tsx
+++ src/app/resume/_components/_tabs/experience-tab.tsx
@@ -1,5 +1,3 @@
-import AnimatedStaggerParent from "@/components/feathers/animated-stagger-parent";
-
 import EXPERIENCE, {
   ExperienceType,
 } from "@/lib/constants/experiences.constants";
@@ -17,32 +15,28 @@ export default function ExperienceTab() {
         and the impact I’ve made throughout my journey in web development.
       </ResumeHeading.description>
 
-      <AnimatedStaggerParent>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4  mt-5 lg:mt-10">
-          {EXPERIENCE.map((item: ExperienceType, index: number) => {
-            return (
-              <div
-                key={index}
-                className=" py-6 bg-[#232329] px-10 rounded-xl flex flex-col items-center justify-between lg:items-start gap-1"
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4  mt-5 lg:mt-10">
+        {EXPERIENCE.map((item: ExperienceType, index: number) => {
+          return (
+            <div
+              key={index}
+              className=" py-6 bg-[#232329] px-10 rounded-xl flex flex-col items-center justify-between lg:items-start gap-1"
+            >
+              <p className="text-accent text-sm">{item.duration}</p>
+              <h3 className="text-xl font-semibold h-18 mb-6">{item.title}</h3>
+              <a
+                href={item.company.link}
+                rel="noopener noreferrer"
+                target="_blank"
+                className="flex items-center gap-2 text-gray-400 text-xs font-bold"
               >
-                <p className="text-accent text-sm">{item.duration}</p>
-                <h3 className="text-xl font-semibold h-18 mb-6">
-                  {item.title}
-                </h3>
-                <a
-                  href={item.company.link}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                  className="flex items-center gap-2 text-gray-400 text-xs font-bold"
-                >
-                  <span className="size-2 rounded-full bg-accent inline-block" />
-                  {item.company.name}
-                </a>
-              </div>
-            );
-          })}
-        </div>
-      </AnimatedStaggerParent>
+                <span className="size-2 rounded-full bg-accent inline-block" />
+                {item.company.name}
+              </a>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
